215150d93abf8ed13a68f0102e8c8c6698204075
acex007x <49668948+acex007x@users.noreply.github.com>
2019-05-25 09:20:13 +0530

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -54,7 +54,7 @@ userSchema.statics.findByCredentials = async (email, password) => {
     }
     const isMatch = await bcrypt.compare(password, user.password)
     if (!isMatch) {
-        throw new Error('wrong credentials')
+        throw new Error('Login failed')
     }
     return user
 }
